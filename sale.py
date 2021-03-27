a8755e9b395892d0b6bc2e6cdce2879cecb60626
Albert Cervera i Areny <albert@nan-tic.com>
2021-03-27 23:41:54 +0100

    Pyflakes fixes.

--- sale.py
+++ sale.py
@@ -7,8 +7,6 @@ from trytond.transaction import Transaction
 from trytond.i18n import gettext
 from trytond.exceptions import UserError
 
-__all__ = ['Sale']
-
 
 class Sale(metaclass=PoolMeta):
     __name__ = 'sale.sale'
